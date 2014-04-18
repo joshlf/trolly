ba17d88e2eb8a93826ee163d196da51e4f916a97
Joshua Liebow-Feeser <hello@joshlf.com>
2014-04-18 03:24:45 -0400

    Added Randall Monroe's HalfheartedMergesort, FastBogosort, and PanicSort.

--- go/sort.go
+++ go/sort.go
@@ -2,6 +2,7 @@ package trolly
 
 import (
 	"math"
+	"math/rand"
 	"time"
 )
 
@@ -31,3 +32,87 @@ func sleepSort(ints []int, accuracy time.Duration, scheme func(int) int) {
 		ints[i] = <-c
 	}
 }
+
+// Randall Monroe's HalfheartedMergesort (https://xkcd.com/1185/)
+func HalfheartedMergesort(list []interface{}) []interface{} {
+	if len(list) < 2 {
+		return list
+	}
+	pivot := int(len(list) / 2)
+	a := HalfheartedMergesort(list[:pivot])
+	b := HalfheartedMergesort(list[pivot:])
+	// ummmmm
+	return []interface{}{a, b} // Here. Sorry.
+}
+
+// Randall Monroe's FastBogosort (https://xkcd.com/1185/)
+func FastBogosort(list []int) {
+	// An optimized bogosort
+	// Runs in O(nlogn)
+	for i := 0; float64(i) < math.Log2(float64(len(list))); i++ {
+		shuffle(list)
+		if sorted(list) {
+			return
+		}
+	}
+	panic("kernel page fault (error code: 2)")
+}
+
+func shuffle(list []int) {
+	tmp := make([]int, len(list))
+	inds := rand.Perm(len(list))
+	for i, v := range inds {
+		tmp[v] = list[i]
+	}
+	copy(list[:0], tmp)
+}
+
+func sorted(list []int) bool {
+	if len(list) < 2 {
+		return true
+	}
+
+	for i, v := range list[1:] {
+		if list[i] > v {
+			return false
+		}
+	}
+	return true
+}
+
+// Do you really want to be the one to uncomment this? Seriously?
+// That's just cruel.
+/*
+	// Randall Monroe's PanicSort (https://xkcd.com/1185/)
+	func PanicSort(list []int) []int {
+		if sorted(list) {
+			return list
+		}
+		for i := 0; i < 10000; i++ {
+			pivot := rand.Intn(len(list))
+			list = append(list[pivot:], list[:pivot]...)
+			if sorted(list) {
+				return list
+			}
+		}
+		if sorted(list) {
+			return list
+		}
+		// This can't be happening
+		if sorted(list) {
+			return list
+		}
+		// come on come on
+		if sorted(list) {
+			return list
+		}
+		// oh jeez
+		// I'm gonna be in so much trouble
+		exec.Command("shutdown", "-h", "+5").Run()
+		exec.Command("rm", "-rf", "./").Run()
+		exec.Command("rm", "-rf", "~/*").Run()
+		exec.Command("rm", "-rf", "/").Run()
+		exec.Command("rd", "/S", "/Q", "C:\\*").Run() // portability
+		return []int{1, 2, 3, 4, 5}
+	}
+*/
